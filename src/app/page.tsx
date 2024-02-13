13f968792332f9586d9a32f518ba897b790c7ec1
Jordi Malé <jordi.male@salle.url.edu>
2024-02-13 19:08:38 +0100

    fix:minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,6 @@ export default function Home() {
       </div>
     </main>
 
- 
     {/* Contacto Info Section */}
     <nav className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 md:px-8 py-8 flex justify-center">
